f4101a8c5c13edd8166bd22de53fd5a363df7355
gas <gasweld@gmail.com>
2017-06-21 00:27:11 +0300

    Use Webpack instead of Gulp

---
+++ bustrack/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	entry: {
+		main: './js/main.js',
+		runtime: './js/runtime.js'
+	},
+	output: {
+		path: __dirname + '/../public/bustrack/res',
+		filename: '[name].js'
+	},
+	module: {
+		rules: [
+			{test: /\.js$/, use: ['babel-loader'], include: __dirname + '/js'}
+		]
+	}
+};
