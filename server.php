ec3a738d46d604a92adbceba2daafed5b3c01c7a
gas <gasweld@gmail.com>
2017-07-03 01:06:05 +0300

    Put public dirs inside the projects

--- server.php
+++ server.php
@@ -1,25 +1,44 @@
 <?php
 
-
 $uri = $_SERVER['REQUEST_URI'];
+$paths = array_slice(explode('/', $uri), 1);
 
-$path = 'public'.$uri;
-
-if(file_exists($path)) {
+// First pathname is a project name
+$project = array_shift($paths);
+$path = implode('/', $paths);
+if (!is_dir($project)) {
 	return false;
 }
+chdir($project.'/public');
 
-$dirs = array_slice(explode('/', $path), 0, 2);
-
-$dir = implode('/', $dirs);
-$entry = $dir.'/index.php';
-if(file_exists($entry)) {
-	chdir($dir);
-	require $entry;
+// Send to <project>/public/<path>
+// or to <project>/public/index.php
+if (file_exists($path)) {
+	$mime = mime($path);
+	header('Content-Type: '.$mime);
+	readfile($path);
 	return true;
 }
 
-return false;
+function mime($path)
+{
+	$types = [
+		'.css' => 'text/css',
+		'.js' => 'text/javascript',
+		'.png' => 'image/png',
+		'.jpg' => 'image/jpeg',
+		'.jpeg' => 'image/jpeg',
+	];
+	foreach ($types as $ext => $type) {
+		$n = strlen($ext);
+		if (substr($path, -$n) == $ext) {
+			return $type;
+		}
+	}
+	return mime_content_type($path);
+}
 
+require 'index.php';
+return true;
 
 ?>
