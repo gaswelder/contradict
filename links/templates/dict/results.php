93f2b2b142be97a5070d553dc0a602e7821c0941
gas <gasweld@gmail.com>
2017-11-22 23:59:19 +0300

    UI tweak

--- links/templates/dict/results.php
+++ links/templates/dict/results.php
@@ -4,7 +4,12 @@
 	<a href="/dict/test">New test</a>
 	<a href="/dict">Home</a>
 </nav>
-
+<style>
+.nope td:nth-child(2) {
+	color: red;
+	text-decoration: line-through;
+}
+</style>
 <table>
 <tr>
 	<th>Q</th>
@@ -13,7 +18,7 @@
 	<th></th>
 </tr>
 <?php foreach ($results as $r): ?>
-	<tr>
+	<tr class="{{$r['ok'] ? 'ok' : 'nope'}}">
 		<td>{{$r['q']}}</td>
 		<td>{{$r['a']}}</td>
 		<td>{{$r['expected']}}</td>
