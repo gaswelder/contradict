8177f0d9effa5da36c49d50909ad489d7bd5f95c
gas <gasweld@gmail.com>
2017-12-17 01:06:05 +0300

    Pages update

--- links/templates/pages.php
+++ links/templates/pages.php
@@ -8,8 +8,7 @@ html {
 }
 
 body {
-	font-size: 12pt;
-	line-height: 1.3em;
+	font-size: 14pt;
 
 	display: flex;
 	height: 100%;
@@ -47,6 +46,21 @@ form textarea {
 	font-size: 11pt;
 	padding: 6pt;
 }
+
+#preview {
+	flex: 1 1 200px;
+	height: calc(100% - 2.5em);
+	overflow-y: scroll;
+	padding: 1em;
+}
+
+#preview pre {
+	padding: 10px;
+	margin-left: 2em;
+	font-size: 90%;
+	font-weight: normal;
+	font-family: monospace;
+}
 </style>
 </head>
 <body>
@@ -62,10 +76,48 @@ form textarea {
 		<div>
 			<button>Save</button>
 		</div>
-
 		<textarea name="content">{{$page->content}}</textarea>
-
 	</form>
 
+	<div id="preview"></div>
+<script src="https://unpkg.com/markdown-it@8.4.0/dist/markdown-it.min.js"></script>
+<script>
+const editor = document.querySelector('textarea');
+const preview = document.getElementById('preview');
+
+var md = window.markdownit();
+
+function sync() {
+	preview.innerHTML = md.render(editor.value);
+}
+
+editor.addEventListener('keydown', function(e) {
+	if (e.key == 'Tab') {
+		e.preventDefault();
+		const t = e.target;
+		const p1 = t.selectionStart;
+		const p2 = t.selectionEnd;
+		t.value = t.value.substr(0, p1) + "\t" + t.value.substr(p2);
+		t.selectionStart = t.selectionEnd = p1 + 1;
+		sync();
+	}
+});
+
+editor.addEventListener('input', function(e) {
+	sync();
+});
+
+sync();
+
+function markdown(md) {
+	return md
+		.replace(/    /g, "\t")
+		.replace(/&/g, '&amp;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;')
+		.replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;")
+		.replace(/\n/g, "<br>\n");
+}
+</script>
 </body>
 </html>
