3ce31978fb8aa1662570ad015fce82e8a237344b
gas <gasweld@gmail.com>
2018-01-05 18:44:28 +0300

    Tweaks for mobile view

--- links/templates/dict/header.php
+++ links/templates/dict/header.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta name="viewport" content="width=device-width">
 <link rel="stylesheet" href="/res/style.css">
 <style>
 body {
 	margin: 2cm;
+}
+
+@media (max-width: 400px) {
+	body {
+		margin: 1em;
+	}
 }
 </style>
 </head>
