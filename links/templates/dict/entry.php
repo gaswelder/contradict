2ed7cce727bbfb8404f1f625a0c529bc716bd50b
gas <gasweld@gmail.com>
2018-02-13 22:42:18 +0300

    Missing template

---
+++ links/templates/dict/entry.php
@@ -0,0 +1,7 @@
+<?= tpl('dict/header') ?>
+
+<form method="post">
+	<input name="q" value="{{$entry->q}}" required>
+	<input name="a" value="{{$entry->a}}" required>
+	<button>Save</button>
+</form>
