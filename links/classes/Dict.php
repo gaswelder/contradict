643f904003ef548b45cd6097b3c5d3298b510291
gas <gasweld@gmail.com>
2018-03-02 22:41:56 +0300

    Exclude archived words from checking

--- links/classes/Dict.php
+++ links/classes/Dict.php
@@ -32,13 +32,12 @@ class Dict
         return Entry::db()->getValue("select count(*) from words where q = ? and a = ?", $e->q, $e->a) > 0;
     }
 
-    // Returns list of entries matching the given question for the given direction.
-    private function entries($dir, $q)
-    {
-        $f = $dir == 0 ? 'q' : 'a';
-        return Entry::fromRows(Entry::db()->getRows("select * from words where lower($f) = ?", mb_strtolower($q)));
-    }
-
+    /**
+     * Checks a given answer, returns a result.
+     *
+     * @param Answer $answer
+     * @return Result
+     */
     function result(Answer $answer)
     {
         $dir = $answer->dir;
@@ -46,7 +45,12 @@ class Dict
         $a = $answer->a;
 
         // Find all rows with this question
-        $entries = $this->entries($dir, $q);
+        $questionField = $dir == 0 ? 'q' : 'a';
+        $timesAnsweredField = $dir == 0 ? 'answers1' : 'answers2';
+        $entries = Entry::fromRows(Entry::db()->getRows("
+            select * from words
+            where lower($questionField) = ?
+            and $timesAnsweredField < ?", mb_strtolower($q), self::GOAL));
 
         // Find one that matches.
         $match = array_reduce($entries, function ($prev, Entry $entry) use ($dir, $a) {
