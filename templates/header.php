f88b4ee1b15f7d0ac45a10d846c66f6299ad147c
gas <gasweld@gmail.com>
2018-05-06 03:36:14 +0300

    Style tweaks

--- templates/header.php
+++ templates/header.php
@@ -6,28 +6,14 @@
 <style>
 body {
 	margin: 2cm;
-	padding-top: 4em;
+	margin-top: 0;
+	padding-top: 1em;
 }
 
 @media (max-width: 400px) {
 	body {
 		margin: 1em;
 	}
-}
-
-#header {
-	background-color: #aae;
-	position: fixed;
-	left: 0;
-	right: 0;
-	top: 0;
-	padding: 0 1em;
-}
-
-#header a {
-	display: inline-block;
-	float: left;
-	margin: 1em 1em 0 0;
 }
 </style>
 
