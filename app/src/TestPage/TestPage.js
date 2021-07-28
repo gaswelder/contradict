a2c4b35ec628cd3b13f4e8fcc29203da3402602b
gasweld <gasweld@gmail.com>
2021-07-29 01:13:07 +0300

    Polish test page

--- app/src/TestPage/TestPage.js
+++ app/src/TestPage/TestPage.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { withRouter } from "react-router";
+import styled from "styled-components";
 import Resource from "../components/Resource";
 import withAPI from "../components/withAPI";
 import { TestSection } from "./Test";
+
+const Form = styled.form`
+  & > div {
+    columns: 20em;
+    border-bottom: 1px dashed #bebedb;
+    padding-bottom: 1em;
+    margin-bottom: 2em;
+  }
+  & > div > section {
+    vertical-align: top;
+    margin-bottom: 1em;
+  }
+  & > div > section > div {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 0.5em;
+  }
+`;
 
 const TestPage = ({ api, match, busy, history }) => {
   const handleSubmit = async (entries) => {
@@ -14,7 +33,7 @@ const TestPage = ({ api, match, busy, history }) => {
   return (
     <Resource getPromise={() => api.test(match.params.id)}>
       {(data) => (
-        <form
+        <Form
           method="post"
           className="test-form"
           onSubmit={(e) => {
@@ -26,14 +45,16 @@ const TestPage = ({ api, match, busy, history }) => {
             handleSubmit(r);
           }}
         >
-          <TestSection tuples={data.tuples1} dir="0" />
-          <TestSection tuples={data.tuples2} dir="1" />
           <div>
-            <button type="submit" disabled={busy}>
-              Submit
-            </button>
+            <TestSection tuples={data.tuples1} dir="0" />
           </div>
-        </form>
+          <div>
+            <TestSection tuples={data.tuples2} dir="1" />
+          </div>
+          <button type="submit" disabled={busy}>
+            Submit
+          </button>
+        </Form>
       )}
     </Resource>
   );
