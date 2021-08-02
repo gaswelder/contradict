8a8c80d67de7a99c31bab9db403690b3bbd4d6f2
gasweld <gasweld@gmail.com>
2021-08-02 23:04:27 +0300

    Rewrite entry page

--- app/src/EntryPage.js
+++ app/src/EntryPage.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import Resource from "./components/Resource";
 import withAPI from "./components/withAPI";
 
 const Form = styled.form`
@@ -8,50 +9,38 @@ const Form = styled.form`
   align-items: center;
 `;
 
-class EntryPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      entry: null,
-      saving: false,
-    };
-    this.submit = this.submit.bind(this);
-  }
+const EntryPage = ({ match, api }) => {
+  const [saving, setSaving] = useState(false);
 
-  async componentDidMount() {
-    const id = this.props.match.params.id;
-    const entry = (await this.props.api.entry(id)).entry;
-    this.setState({ entry });
-  }
+  const id = match.params.id;
 
-  async submit(e) {
+  const submit = async (e) => {
     e.preventDefault();
     const f = e.target;
     const q = f.querySelector("[name=q]").value;
     const a = f.querySelector("[name=a]").value;
-    this.setState({ saving: true });
-    const id = this.props.match.params.id;
-    await this.props.api.updateEntry(id, { q, a });
-    this.setState({ saving: false });
-  }
+    setSaving(true);
+    await api.updateEntry(id, { q, a });
+    setSaving(false);
+  };
 
-  render() {
-    const { entry, saving } = this.state;
-    if (!entry) return "Loading...";
-    return (
-      <Form method="post" onSubmit={this.submit}>
-        <div>
-          <label>Q</label>
-          <input name="q" defaultValue={entry.q} required />
-        </div>
-        <div>
-          <label>A</label>
-          <input name="a" defaultValue={entry.a} required />
-        </div>
-        <button disabled={saving}>Save</button>
-      </Form>
-    );
-  }
-}
+  return (
+    <Resource getPromise={() => api.entry(id).then((r) => r.entry)}>
+      {(entry) => (
+        <Form method="post" onSubmit={submit}>
+          <div>
+            <label>Q</label>
+            <input name="q" defaultValue={entry.q} required />
+          </div>
+          <div>
+            <label>A</label>
+            <input name="a" defaultValue={entry.a} required />
+          </div>
+          <button disabled={saving}>Save</button>
+        </Form>
+      )}
+    </Resource>
+  );
+};
 
 export default withAPI(EntryPage);
