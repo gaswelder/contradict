f3a22275f74705f1a43657c6cbc7dd7fd2aded6b
gasweld <gasweld@gmail.com>
2023-02-18 17:15:01 +0200

    Remove old templates

--- backend/templates/add.php
+++
@@ -1,9 +0,0 @@
-<?= tpl('header') ?>
-<form method="post">
-	<div>
-		<textarea name="words" id="" cols="30" rows="10"></textarea>
-	</div>
-	<div>
-		<button>Add</button>
-	</div>
-</form>
--- backend/templates/entry.php
+++
@@ -1,7 +0,0 @@
-<?= tpl('header') ?>
-
-<form method="post">
-	<input name="q" value="{{$entry['q']}}" required>
-	<input name="a" value="{{$entry['a']}}" required>
-	<button>Save</button>
-</form>
