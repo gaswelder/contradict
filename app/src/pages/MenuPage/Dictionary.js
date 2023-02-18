a8be6b3582cea87d5efc27b972b5bd12fdaf8722
gasweld <gasweld@gmail.com>
2023-02-19 00:58:20 +0200

    Rework the main page

--- app/src/pages/MenuPage/Dictionary.js
+++ app/src/pages/MenuPage/Dictionary.js
@@ -2,67 +2,64 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { ButtonLink } from "../../components/ButtonLink";
-import ColumnMeter from "../../components/ColumnMeter";
+import "./ColumnMeter.css";
 
-const Container = styled.section`
-  display: inline-block;
+const RootDiv = styled.section`
   padding: 1em;
   margin-bottom: 0.5em;
-  & dl {
-    display: grid;
-    grid-template-columns: max-content max-content;
-  }
-  & dd {
-    text-align: right;
-  }
+  display: inline-block;
+  margin-right: 0.5em;
+  border-radius: 4px;
 `;
 
-const EditLinks = styled.div`
+const HeaderDiv = styled.div`
   font-size: smaller;
   & a + a {
     margin-left: 0.3em;
   }
+  padding: 1em 0;
+  display: flex;
+  & h3 {
+    margin: 0 auto 0 0;
+    font-size: 14pt;
+  }
 `;
 
 function Dictionary({ dict }) {
+  const { pairs, touched, finished } = dict.stats;
+  const total = pairs + finished;
+  const pos = (n) => (100 * n) / total + "%";
   return (
-    <Container>
-      <h3>{dict.name}</h3>
-      <EditLinks>
+    <RootDiv>
+      <HeaderDiv>
+        <h3>{dict.name}</h3>
         <Link to={`dicts/${dict.id}`}>Edit</Link>
         <Link to={`/${dict.id}/add`}>Add words</Link>
-      </EditLinks>
-      <Stats stats={dict.stats} />
+      </HeaderDiv>
+      <div className="column-meter">
+        <div
+          className="finished"
+          style={{ width: pos(finished) }}
+          title="finished"
+        >
+          {finished}
+        </div>
+        <div
+          className="in-progress"
+          style={{ width: pos(touched) }}
+          title="touched"
+        >
+          {finished + touched}
+        </div>
+        <div className="in-queue" style={{ width: pos(total) }} title="total">
+          {total}
+        </div>
+      </div>
       <div style={{ marginTop: "0.5em" }}>
         <ButtonLink to={`/${dict.id}/test`}>Do a test</ButtonLink>{" "}
         <ButtonLink to={`/${dict.id}/repetitions`}>Do repetitions</ButtonLink>{" "}
       </div>
-    </Container>
-  );
-}
-
-function Stats(props) {
-  const { stats } = props;
-  const { pairs, touched, finished } = stats;
-
-  return (
-    <div>
-      <dl>
-        <dt>Number of entries</dt>
-        <dd>{pairs}</dd>
-
-        <dt>Finished</dt>
-        <dd>{finished}</dd>
-
-        <dt>Touched</dt>
-        <dd>{touched}</dd>
-      </dl>
-      <ColumnMeter
-        inQueue={pairs - touched}
-        inProgress={touched}
-        finished={finished}
-      />
-    </div>
+    </RootDiv>
   );
 }
 
