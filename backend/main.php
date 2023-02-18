3d4c859b62d86a3f9b6d5dc652090dc79d43b9c0
gasweld <gasweld@gmail.com>
2023-02-18 22:13:37 +0200

    Remove a log

--- backend/main.php
+++ backend/main.php
@@ -3,7 +3,6 @@ require __DIR__ . '/../vendor/autoload.php';
 
 spl_autoload_register(function ($cn) {
     $path = str_replace('//', '/', __DIR__ . "/classes/" . str_replace('\\', '//', $cn) . ".php");
-    error_log($path);
     if (file_exists($path)) {
         require_once $path;
     }
