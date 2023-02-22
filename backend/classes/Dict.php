966d4c6ff4c91e268791001a941f14538262bc33
gasweld <gasweld@gmail.com>
2023-02-22 12:37:47 +0200

    Remove unused code

--- backend/classes/Dict.php
+++ backend/classes/Dict.php
@@ -16,21 +16,4 @@ class Dict
         $d->lookupURLTemplates = $arr['lookupURLTemplates'] ?? [];
         return $d;
     }
-
-    function saveEntry(Entry $e): Entry
-    {
-        if (!$e->id) {
-            $e->id = uniqid();
-        }
-        $this->data['words'][$e->id] = [
-            'q' => $e->q,
-            'a' => $e->a,
-            'answers1' => $e->answers1,
-            'answers2' => $e->answers2,
-            'id' => $e->id,
-            'dict_id' => $e->dict_id,
-            'touched' => $e->touched ? 1 : 0,
-        ];
-        return $e;
-    }
 }
