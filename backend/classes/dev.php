18e118ed98eba35ced92e7924d2ad4f5ac3b2d92
gasweld <gasweld@gmail.com>
2023-02-26 02:27:03 +0200

    Improve clg output

--- backend/classes/dev.php
+++ backend/classes/dev.php
@@ -7,13 +7,11 @@ class dev
         ob_start();
         var_dump($var);
         $s = ob_get_clean();
-        return $s;
+        return trim($s);
     }
 
     static function clg(...$var)
     {
-        foreach ($var as $i => $var) {
-            error_log("$i ---> " . self::varfmt($var));
-        }
+        error_log(implode(' ', array_map([self::class, 'varfmt'], $var)));
     }
 }
