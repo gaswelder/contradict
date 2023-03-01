a16124bb08e2d4edaa8bb0acb087e81b91a6b7df
gasweld <gasweld@gmail.com>
2023-03-02 01:18:03 +0200

    Fix login redirect

--- app/src/pages/LoginPage.js
+++ app/src/pages/LoginPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import api from "../api";
+import api, { ROOT_PATH } from "../api";
 import { withRouter } from "react-router";
 import styled from "styled-components";
 
@@ -11,43 +11,36 @@ const Form = styled.form`
   align-items: center;
 `;
 
-class LoginPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  async handleSubmit(e) {
+const LoginPage = ({ history }) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const form = e.target;
     const login = form.querySelector('[name="login"]').value;
     const password = form.querySelector('[name="password"]').value;
     try {
       await api.login(login, password);
-      this.props.history.push("/");
+      history.push(ROOT_PATH);
     } catch (e) {
       alert(e.toString());
     }
-  }
+  };
 
-  render() {
-    return (
-      <Form method="post" id="login-form" onSubmit={this.handleSubmit}>
-        <div>
-          <input name="login" autoFocus required placeholder="Login" />
-        </div>
-        <div>
-          <input
-            type="password"
-            name="password"
-            required
-            placeholder="Password"
-          />
-        </div>
-        <button type="submit">Login</button>
-      </Form>
-    );
-  }
-}
+  return (
+    <Form method="post" id="login-form" onSubmit={handleSubmit}>
+      <div>
+        <input name="login" autoFocus required placeholder="Login" />
+      </div>
+      <div>
+        <input
+          type="password"
+          name="password"
+          required
+          placeholder="Password"
+        />
+      </div>
+      <button type="submit">Login</button>
+    </Form>
+  );
+};
 
 export default withRouter(LoginPage);
